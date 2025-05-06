5054b9155f02fc758f000c09bd8982ed234a2b4c
ricky romero <contact@romeroricky.com>
2025-05-06 11:20:30 -0700

    app mobile-b importing packages

--- apps/mobile-b/App.tsx
+++ apps/mobile-b/App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TestModule  from '@repo/test-expo-module';
+import { greeting } from '@repo/utils';
+import { Button } from '@repo/components';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{greeting}</Text>
+      <Text>PACKAGE TestModule</Text>
+      <Text>{TestModule.hello()}</Text>
+      <Button text="Click me" onPress={() => {}} />
     </View>
   );
 }
